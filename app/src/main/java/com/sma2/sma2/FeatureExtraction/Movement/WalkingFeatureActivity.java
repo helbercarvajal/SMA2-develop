ae72c04d13be8630a153b422b397911dbda11300
helbercarvajal <handres.carvajal@gmail.com>
2019-08-21 14:56:44 -0500

    Add radar chart to walking results

--- app/src/main/java/com/sma2/sma2/FeatureExtraction/Movement/WalkingFeatureActivity.java
+++ app/src/main/java/com/sma2/sma2/FeatureExtraction/Movement/WalkingFeatureActivity.java
@@ -1,7 +1,5 @@
 package com.sma2.sma2.FeatureExtraction.Movement;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -21,6 +19,24 @@ import com.sma2.sma2.R;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+
+import android.graphics.Color;
 
 public class WalkingFeatureActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -117,6 +133,47 @@ public class WalkingFeatureActivity extends AppCompatActivity implements View.On
         //Ylabel = getResources().getString(R.string.frequency);
         Ylabel = "degrees";
         graphManager.LineGraph(graph3, x, y, 0, 10, Title, Xlabel, Ylabel);
+
+        // Radar chart
+        RadarChart radarchart= findViewById(R.id.chart2);
+        radarchart.getDescription().setEnabled(false);
+        radarchart.animateXY(5000, 5000, Easing.EaseInOutQuad);
+
+        RadarData radardata= new RadarData();
+        float[] datos1={100f,20f,90f,80f}; // datos que se van a graficar
+        float[] datos2={70f,70f,30f,90f};
+        double area1=0.5*(datos1[0]+datos1[2])*(datos1[1]+datos1[3]);
+        double area2=0.5*(datos2[0]+datos2[2])*(datos2[1]+datos2[3]);
+        radardata=setdata(datos1,datos2);
+        String[] labels={"Tremor","Max. Aceleration", "Max. Velocity", "Energy"};
+
+        XAxis xAxis=radarchart.getXAxis();
+        xAxis.setTextSize(12f);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setLabelRotationAngle(90f);
+
+        YAxis yAxis = radarchart.getYAxis();
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(80f);
+
+        Legend l = radarchart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+        l.setTextColor(Color.BLACK);
+        l.setTextSize(20f);
+
+        radarchart.setExtraOffsets(0,-400,0,-400);
+    //radarchart.setBackgroundColor(Color.WHITE);
+        radarchart.setScaleY(1f);
+        radarchart.setScaleX(1f);
+        radarchart.setData(radardata);
+        radarchart.invalidate(); // refresh
+
     }
 
     @Override
@@ -133,6 +190,50 @@ public class WalkingFeatureActivity extends AppCompatActivity implements View.On
         Intent i =new Intent(WalkingFeatureActivity.this, MainActivityMenu.class);
         startActivity(i);
 
+    }
+
+    private RadarData setdata(float[] datos1, float[] datos2) {
+        int cnt = datos1.length;
+        ArrayList<RadarEntry> entries1 = new ArrayList<RadarEntry>();
+        ArrayList<RadarEntry> entries2 = new ArrayList<RadarEntry>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < cnt; i++) {
+            //float val1 = (float) (Math.random() * mul) + min;
+            entries1.add(new RadarEntry(datos1[i]));
+
+            //float val2 = (float) (Math.random() * mul) + min;
+            entries2.add(new RadarEntry(datos2[i]));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "Below Session");
+        set1.setColor(Color.rgb(255, 185, 0));
+        set1.setFillColor(Color.rgb(255, 185, 0));
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(200);
+        set1.setLineWidth(2f);
+        set1.setValueTextColor(Color.rgb(255, 185, 0));
+        set1.setValueTextSize(15f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+        RadarDataSet set2 = new RadarDataSet(entries2, "Current Session");
+        set2.setColor(Color.rgb(0, 200, 200));
+        set2.setFillColor(Color.rgb(0, 200, 200));
+        set2.setDrawFilled(true);
+        set2.setFillAlpha(180);
+        set2.setLineWidth(2f);
+        set2.setDrawHighlightCircleEnabled(true);
+        set2.setDrawHighlightIndicators(false);
+        set2.setValueTextColor(Color.rgb(0, 200, 200));
+        set2.setValueTextSize(15f);
+
+        RadarData dataradar= new RadarData();
+
+        dataradar.addDataSet(set1);
+        dataradar.addDataSet(set2);
+        return dataradar;
     }
 
 }
